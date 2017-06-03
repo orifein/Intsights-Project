c44f01cd2cf8195948692046e2b97ff9fcc1928d
orifein <orifein@gmail.com>
2017-06-03 15:45:50 +0300

    Add files via upload

--- intsights/intsights_crawler.py
+++ intsights/intsights_crawler.py
@@ -20,39 +20,56 @@ class IntsightsCrawler():
         tree = etree.parse(StringIO(content), parser)
         return tree
 
-    def _fetch_date_and_author_from_element(self, element):
+    def _fetch_date_and_author(self, element):
+        fetched_text = ''
         element_text = element.cssselect('.pre-footer')[0]
-        fetched_text = ''.join([x.replace('\n', '').replace('\t', '') for x in element_text.itertext()])
-        fetched_text_splitted = fetched_text.replace(',', '').split(' ')
-        author = fetched_text_splitted[2]
+        for x in element_text.itertext():
+            fetched_text += x.replace('\n', '').replace('\t', '')
+        fetch_splitted = fetched_text.replace(',', '').split(' ')
+        author = fetch_splitted[2]
         if author in self.AUTHOR_NAMES_CONST:
             author = 'Anonymous'
-        date = arrow.get(fetched_text_splitted[6]+'-'+fetched_text_splitted[5]+'-'+fetched_text_splitted[4]+' '+fetched_text_splitted[7], 'YYYY-MMM-DD HH:mm:ss')
+        date_fetched = fetch_splitted[6]+'-'+fetch_splitted[5] + \
+            '-'+fetch_splitted[4]+' '+fetch_splitted[7]
+        date = arrow.get(date_fetched, 'YYYY-MMM-DD HH:mm:ss')
         return author, date
 
     def _get_elements_from_tree(self, tree):
+        content = ''
         list_of_models = []
         html_root = tree.getroot()
         all_rows = html_root.cssselect(self.ALL_ROWS_ELEMENTS)
         for element in all_rows:
             response = IntsightsModel()
             got_title = element.cssselect('h4')
-            content_text = element.cssselect('.well-sm')
+            txt_cont = element.cssselect('.well-sm')
             if not got_title:
                 continue
-            response.title = got_title[0].text.replace('\n', '').replace('\t', '')
-            response.content = ''.join([txt.replace('\n', '').replace('\t', '') for txt in content_text[0].itertext()])
-            response.author, response.date = self._fetch_date_and_author_from_element(element)
+            title_text = got_title[0].text
+            response.title = title_text.replace('\n', '').replace('\t', '')
+            for txt in txt_cont[0].itertext():
+                content += txt.replace('\n', '').replace('\t', '')
+            response.content = content
+            author, date = self._fetch_date_and_author(element)
+            response.author = author
+            response.date = date
             list_of_models.append(response)
-        list_of_models.sort(key=lambda model: model.date, reverse=True)  # sorting all by dates
+        # sorting dates
+        list_of_models.sort(key=lambda model: model.date, reverse=True)
         for paste_model in list_of_models:
             paste_model.date = str(paste_model.date)
         return list_of_models
 
+    def _get_proxy_for_onion_sites(self):
+        session = requests.session()
+        session.proxies = {'http':  'socks5h://localhost:9050',
+                           'https': 'socks5h://localhost:9050'}
+        return session
+
     def crawl(self):
+        session = self._get_proxy_for_onion_sites()
         #       content = self._get_content_from_html()
-        enc = 'utf-8'
-        content = open('/Users/orifein/Downloads/StrongholdPaste.html', encoding=enc).read()
+        content = session.get('http://nzxj65x32vh2fkhk.onion/all').text
         tree = self._parse_html_content(content)
         response = self._get_elements_from_tree(tree)
         return response
